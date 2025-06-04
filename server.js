025f50b1615d63167a03b523d7157b99e43f1521
glebkoxan36 <glebkoxan36@gmail.com>
2025-06-04 22:48:18 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const qs = require('qs'); // библиотека для сериализации
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Простой ответ на GET /
-app.get('/', (req, res) => {
-  res.send('Proxy server is running');
-});
-
 app.post('/proxy', async (req, res) => {
   try {
-    const response = await axios.post('https://api.ifreeicloud.co.uk', req.body);
+    const formData = qs.stringify(req.body);
+
+    const response = await axios.post('https://api.ifreeicloud.co.uk', formData, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Proxy error', details: error.message });
