d8a3b35634194ada7f168d6e0b88bc1c89a686b8
glebkoxan36 <glebkoxan36@gmail.com>
2025-06-04 22:39:38 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
@@ -6,6 +5,11 @@ const axios = require('axios');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Простой ответ на GET /
+app.get('/', (req, res) => {
+  res.send('Proxy server is running');
+});
 
 app.post('/proxy', async (req, res) => {
   try {
